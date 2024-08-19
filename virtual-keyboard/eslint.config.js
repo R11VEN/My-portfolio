61f8a4a3b09c88bd81110a20f860f43fe5335dbf
Max Komarov <b-raven@yandex.ru>
2024-08-19 19:08:10 +0300

    add prettier to project

--- virtual-keyboard/eslint.config.js
+++ virtual-keyboard/eslint.config.js
@@ -2,30 +2,17 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
     rules: {
-      'indent': [
-        'error',
-        2
-      ],
-      'linebreak-style': [
-        'error',
-        'unix'
-      ],
-      'quotes': [
-        'error',
-        'single'
-      ],
-      'semi': [
-        'error',
-        'always'
-      ],
-    }
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
   },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
