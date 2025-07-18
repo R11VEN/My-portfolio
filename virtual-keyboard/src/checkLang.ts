0c9bef0d8385e46b02ac854de96397341f2e40c4
Max Komarov <b-raven@yandex.ru>
2025-07-18 17:45:13 +0300

    fix condition

--- virtual-keyboard/src/checkLang.ts
+++ virtual-keyboard/src/checkLang.ts
@@ -3,15 +3,13 @@ import { keys } from './keys.ts';
 
 export const checkLang = (event: KeyboardEvent) => {
   let lang = localStorage.getItem('lang');
-  if (event.key.match(/[a-zA-Z]/) && !(event.code in keys.special) && lang !== 'eng') {
+  if (event.key.match(/[a-zA-Z]/) && !(event.code in keys.special)) {
     localStorage.setItem('lang', 'eng');
     lang = 'eng';
     switchLang(lang);
-  } else if (event.key.match(/[а-яёА-ЯЁ]/) && lang !== 'ru') {
+  } else if (event.key.match(/[а-яёА-ЯЁ]/) && !(event.code in keys.special)) {
     localStorage.setItem('lang', 'ru');
     lang = 'ru';
     switchLang(lang);
-  } else {
-    console.log('loh');
   }
 };
