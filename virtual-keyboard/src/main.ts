0b2775795552db5cb60fc707d060ece99ec0a750
Max Komarov <b-raven@yandex.ru>
2025-07-18 17:43:04 +0300

    add textarea id & description

--- virtual-keyboard/src/main.ts
+++ virtual-keyboard/src/main.ts
@@ -6,13 +6,14 @@ import { mouseEvents } from './mouseEvents';
 document.querySelector<HTMLDivElement>('body')!.innerHTML = `
   <div id="app">
     <p class="title">Виртуальная клавиатура</p>
-    <textarea class="textarea"></textarea>
+    <textarea class="textarea" id="textarea"></textarea>
     <div class="keyboard"></div>
     <p class="description">Клавиатура создана для операционной системы Windows</p>
-    <p class="language">Для переключения языка используйте комбинацию: зажать Ctrl и нажать Alt</p>
+    <p class="language">Для переключения языка используйте комбинацию: зажать Ctrl и нажать Alt.</p>
+    <p class="language">Доступна только русская и английская раскладка.</p>
     <div class="massage_container">
       <p class="massage">Проверяем, нажат ли CapsLock</p>
-      <button class="close_button">close</button>
+      <button class="close_button">Close</button>
     </div>
   </div>
 `;
