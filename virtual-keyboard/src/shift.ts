1ccb2930200f6d729a5ede9744ee93d6ad1932e5
Max Komarov <b-raven@yandex.ru>
2025-07-18 17:52:02 +0300

    add ru language

--- virtual-keyboard/src/shift.ts
+++ virtual-keyboard/src/shift.ts
@@ -3,42 +3,48 @@ import { keys } from './keys.ts';
 class Shift {
 
   down(buttons: NodeListOf<Element>, event: KeyboardEvent | MouseEvent) {
-    for (let i = 0; i < buttons.length; i++) {
-      keys.eng.shift.up.find((item, index) => {
-        if (item === (<HTMLDivElement>buttons[i]).innerText) {
-          (<HTMLDivElement>buttons[i]).textContent = keys.eng.shift.down[index];
-          (<HTMLDivElement>buttons[i]).classList.add(keys.eng.shift.down[index]);
-        }
-      });
-      keys.eng.shift.keys.find((item, index) => {
-        if (item === (<HTMLDivElement>buttons[i]).innerText.toLowerCase()) {
-          if (event.getModifierState('CapsLock')) {
-            (<HTMLDivElement>buttons[i]).textContent = keys.eng.shift.keys[index].toLowerCase();
-          } else {
-            (<HTMLDivElement>buttons[i]).textContent = keys.eng.shift.keys[index].toUpperCase();
+    const lang = localStorage.getItem('lang');
+    if (lang === 'ru' || lang === 'eng') {
+      for (let i = 0; i < buttons.length; i++) {
+        keys[lang].shift.up.find((item, index) => {
+          if (item === (<HTMLDivElement>buttons[i]).innerText) {
+            (<HTMLDivElement>buttons[i]).textContent = keys[lang].shift.down[index];
+            (<HTMLDivElement>buttons[i]).classList.add(keys[lang].shift.down[index]);
           }
-        }
-      });
+        });
+        keys[lang].shift.keys.find((item, index) => {
+          if (item === (<HTMLDivElement>buttons[i]).innerText.toLowerCase()) {
+            if (event.getModifierState('CapsLock')) {
+              (<HTMLDivElement>buttons[i]).textContent = keys[lang].shift.keys[index].toLowerCase();
+            } else {
+              (<HTMLDivElement>buttons[i]).textContent = keys[lang].shift.keys[index].toUpperCase();
+            }
+          }
+        });
+      }
     }
   }
 
   up(buttons: NodeListOf<Element>, event: KeyboardEvent | MouseEvent) {
-    for (let i = 0; i < buttons.length; i++) {
-      keys.eng.shift.down.find((item, index) => {
-        if (item === (<HTMLDivElement>buttons[i]).innerText) {
-          (<HTMLDivElement>buttons[i]).textContent = keys.eng.shift.up[index];
-          (<HTMLDivElement>buttons[i]).classList.remove(keys.eng.shift.down[index]);
-        }
-      });
-      keys.eng.shift.keys.find((item, index) => {
-        if (item === (<HTMLDivElement>buttons[i]).innerText.toLowerCase()) {
-          if (event.getModifierState('CapsLock')) {
-            (<HTMLDivElement>buttons[i]).textContent = keys.eng.shift.keys[index].toUpperCase();
-          } else {
-            (<HTMLDivElement>buttons[i]).textContent = keys.eng.shift.keys[index].toLowerCase();
+    const lang = localStorage.getItem('lang');
+    if (lang === 'ru' || lang === 'eng') {
+      for (let i = 0; i < buttons.length; i++) {
+        keys[lang].shift.down.find((item, index) => {
+          if (item === (<HTMLDivElement>buttons[i]).innerText) {
+            (<HTMLDivElement>buttons[i]).textContent = keys[lang].shift.up[index];
+            (<HTMLDivElement>buttons[i]).classList.remove(keys[lang].shift.down[index]);
           }
-        }
-      });
+        });
+        keys[lang].shift.keys.find((item, index) => {
+          if (item === (<HTMLDivElement>buttons[i]).innerText.toLowerCase()) {
+            if (event.getModifierState('CapsLock')) {
+              (<HTMLDivElement>buttons[i]).textContent = keys[lang].shift.keys[index].toUpperCase();
+            } else {
+              (<HTMLDivElement>buttons[i]).textContent = keys[lang].shift.keys[index].toLowerCase();
+            }
+          }
+        });
+      }
     }
   }
 }
